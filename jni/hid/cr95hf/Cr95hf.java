346c2b22d2a8b26a9852075918a69b731b684ec9
Alhen <alexandr.g.koryagin@gmail.com>
2013-09-20 10:06:53 +0600

    simple change for git testing

--- jni/hid/cr95hf/Cr95hf.java
+++ jni/hid/cr95hf/Cr95hf.java
@@ -6,7 +6,9 @@ public class Cr95hf{
 
     public static void main (String[] args){
         Cr95hf cr95hf = new Cr95hf();
+        System.out.println("READING DEMO-CR95hf-A");
         System.out.println("UID "+cr95hf.getUID());
+
     }
     static{
         System.loadLibrary("Cr95hf");
